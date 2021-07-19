4cc9c0d38919068b301a751374f7dd4d55a1e312
deepaklav <61404809+deepaklav@users.noreply.github.com>
2021-07-19 12:11:54 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo apt-get install python3-pyqt5
+sudo apt-get install python3-pyqt5 -y
 pip3 install numpy==1.16.2
 pip3 install pyqtgraph==0.11.0
 sudo apt-get install libatlas-base-dev
